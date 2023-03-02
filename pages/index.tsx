f6c8296674dc65337d8088d1bc55aacac064ceb5
amir-mohammad <amirm.azarbashi@gmail.com>
2023-03-02 12:04:51 +0330

    add productItme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from "next";
-
-import Image from "next/image";
 import "tailwindcss/tailwind.css";
 import Layout from "../components/layout";
-
+import ProductItem from "../components/ProductItem";
+import { ProductType } from "../types/product.type";
+import { data } from "../utils/data";
 const Home: NextPage = () => {
-  return <Layout title="homepage">Home Page</Layout>;
+  return (
+    <Layout title="homepage">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product: ProductType) => (
+          <ProductItem product={product} key={product.slug} />
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/**/*.{html,js,ts,tsx}"],
+  content: [
+    "./components/**/*.{html,js,ts,tsx}",
+    "./pages/**/*.{html,js,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
